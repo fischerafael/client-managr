82945adef7e70ca18dbfd9494af82df4d688c35b
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-02-23 19:37:34 -0300

    feat: add Client class

---
+++ src/database/Client/index.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema({
+  name: String,
+  address: String,
+  city: String,
+  state: String,
+  country: String,
+  postalCode: String,
+  fiscalNumber: String,
+});
+
+const Client = mongoose.models.Client || mongoose.model("Client", Schema);
+
+export { Client };
